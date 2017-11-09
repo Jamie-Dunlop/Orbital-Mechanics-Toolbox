2bb9262eed694a5a3c78640247037838953362e8
Jamie-Dunlop <32314639+Jamie-Dunlop@users.noreply.github.com>
2017-11-09 11:31:16 +0000

    test

--- Runge-Kutta4.py
+++ Runge-Kutta4.py
@@ -3,33 +3,20 @@
 
 import math
 
-def rk4(f, x0, y0, x1, h):
+def rk4(f, x, y, h):
 
-    x[0] = x = x0
-    y[0] = y = y0
+    k1 = h * f(x, y)
+    k2 = h * f(x + (h/2), y + (k1/2))
+    k3 = h * f(x + (h/2), y + (k2/2))
+    k4 = h * f(x + h, y + k3)
 
-    for i in range (0, n + 1):
+    fx = x + (h)
+    fy = y + ((1/6) * (k1 + 2*k2 + 2*k3 + k4))
 
-        k1 = h * f(x, y)
-        k2 = h * f(x + (h/2), y + (k1/2))
-        k3 = h * f(x + (h/2), y + (k2/2))
-        k4 = h * f(x + h, y + k3)
-
-        x[i] = x = x0 + (i * h)
-        y[i] = y = y0 + ((1/6) * (k1 + 2*k2 + 2*k3 + k4))
-
-    return x, y
+    return fx, fy
 
 def f(x, y):
-    #return (5 * x**2 - y) / math.exp(x + y)
     return x * math.sqrt(y)
 
-x, y = rk4(f, 0, 1, 20, 0.1)
-for x, y in list (zip (x, y)) [::10]:
-
-    # within %number.decimalf
-    # 'decimal' controls the number of decimal places
-    # 'number' controls the spacing between printed numbers when printed
-    # can write %+10.1e to give in scientific or %10.1f to give in decimal
-
-    print (" %4.1f %10.3f %12.3f" % (x, y, x * math.sqrt(y)))
+fx, fy = rk4(f, 0, 1, 0.1)
+print(fx, fy)
