99d09932b51415259289e0d93dee64e58a5e5115
chrisrlowther <chrislowther19@gmail.com>
2017-11-09 13:40:22 +0000

    Works with function x*sqrt(y)

--- Runge-Kutta4.py
+++ Runge-Kutta4.py
@@ -2,16 +2,26 @@
 #Runge-Kutta 4th order
 
 import math
+import numpy as np
+#import Kep2Cart or correct terminology
+
+fx = []
+fy = []
 
 def rk4(f, x, y, h):
 
-    k1 = h * f(x, y)
-    k2 = h * f(x + (h/2), y + (k1/2))
-    k3 = h * f(x + (h/2), y + (k2/2))
-    k4 = h * f(x + h, y + k3)
+    for i in range (0, 200):
 
-    fx = x + (h)
-    fy = y + ((1/6) * (k1 + 2*k2 + 2*k3 + k4))
+        k1 = h * f(x, y)
+        k2 = h * f(x + (h/2), y + (k1/2))
+        k3 = h * f(x + (h/2), y + (k2/2))
+        k4 = h * f(x + h, y + k3)
+
+        fx.append(x + h)
+        fy.append(y + ((1/6) * (k1 + 2*k2 + 2*k3 + k4)))
+
+        x = fx[i]
+        y = fy[i]
 
     return fx, fy
 
@@ -19,4 +29,7 @@ def f(x, y):
     return x * math.sqrt(y)
 
 fx, fy = rk4(f, 0, 1, 0.1)
-print(fx, fy)
+
+arr = np.array(fx)
+
+print(arr, fy)
