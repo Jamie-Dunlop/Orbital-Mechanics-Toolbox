efd5ecdb35c4e931a3ea8d428bff7929adff6b73
chrisrlowther <chrislowther19@gmail.com>
2017-11-21 14:52:20 +0000

    Even more issues than last time...

--- Runge-Kutta4.py
+++ Runge-Kutta4.py
@@ -14,20 +14,16 @@ fv = []
 fr = []
 ft = []
 
-r_vec = np.array([-10515.45, -5235.37, 49.17]) #m
-v_vec = np.array([-2.10305, -4.18146, -5.563290]) #km/s
+r_0 = np.array([-10515.45, -5235.37, 49.17]) #m
+v_0 = np.array([-2.10305, -4.18146, -5.563290]) #km/s
 
-r = np.linalg.norm(r_vec)
+r = np.linalg.norm(r_0)
 
 mu = 3.985992e14 #units
 
-ax = (-mu * r_vec[0]) / (r ** 3)
-ay = (-mu * r_vec[1]) / (r ** 3)
-az = (-mu * r_vec[2]) / (r ** 3)
+a = []
 
-a = np.array([ax, ay, az])
-
-def rk4(a, r_vec, v_vec, t, h):
+def rk4(a, r, r_vec, v_vec, t, h):
 
     #frx = np.empty((0,1))
     #fry = np.empty((0,1))
@@ -35,14 +31,48 @@ def rk4(a, r_vec, v_vec, t, h):
 
     for i in range (0, t):
 
-        k1v = a * r_vec
-        k1r = v_vec
-        k2v = a * (r_vec + (k1r * (h/2)))
-        k2r = v_vec * k1v * (h/2)
-        k3v = a * (r_vec + (k2r * (h/2)))
-        k3r = v_vec * k2v * (h/2)
-        k4v = a * (r_vec + (k3r * h))
-        k4r = v_vec * k3v * h
+        if i == 0:
+
+            r_scal = np.linalg.norm(r)
+
+            a_x = (-mu * r_vec[0]) / (r_scal ** 3)
+            a_y = (-mu * r_vec[1]) / (r_scal ** 3)
+            a_z = (-mu * r_vec[2]) / (r_scal ** 3)
+
+            a = np.array([a_x, a_y, a_z])
+
+            k1v = a * r_vec
+            k1r = v_vec
+            k2v = a * (r_vec + (k1r * (h/2)))
+            k2r = v_vec * k1v * (h/2)
+            k3v = a * (r_vec + (k2r * (h/2)))
+            k3r = v_vec * k2v * (h/2)
+            k4v = a * (r_vec + (k3r * h))
+            k4r = v_vec * k3v * h
+
+        else:
+
+            r_scal = np.linalg.norm(r)
+
+            #index_x = 0 + (3)
+            #index_y = 1 + (3)
+            #index_z = 2 + (3)
+
+            a_x = (-mu * r[i, 0]) / (r_scal ** 3)
+            a_y = (-mu * r[i, 1]) / (r_scal ** 3)
+            a_z = (-mu * r[i, 2]) / (r_scal ** 3)
+
+            a = np.array([a_x, a_y, a_z])
+
+            k1v = a * r
+            k1r = v
+            k2v = a * (r + (k1r * (h/2)))
+            k2r = v * k1v * (h/2)
+            k3v = a * (r + (k2r * (h/2)))
+            k3r = v * k2v * (h/2)
+            k4v = a * (r + (k3r * h))
+
+            print(r)
 
         ft.append(t + h)
         fr.append(r_vec + ((1/6) * (k1r + 2*k2r + 2*k3r + k4r)))
@@ -57,7 +87,7 @@ def rk4(a, r_vec, v_vec, t, h):
 
     return fr, fv
 
-fr, fv = rk4(a, r_vec, v_vec, 1000, 1)
+fr, fv = rk4(a, r, r_0, v_0, 3, 1)
 
 print(fr)
 #ax.plot(fr)
