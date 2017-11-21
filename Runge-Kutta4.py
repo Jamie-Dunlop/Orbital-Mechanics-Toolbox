24b89f8cc394bb676bd70565112ee413a4c7ecaf
chrisrlowther <chrislowther19@gmail.com>
2017-11-21 15:50:24 +0000

    Getting better...

--- Runge-Kutta4.py
+++ Runge-Kutta4.py
@@ -23,21 +23,19 @@ mu = 3.985992e14 #units
 
 a = []
 
-def rk4(a, r, r_vec, v_vec, t, h):
+def rk4(a, r, r_vec, v_vec, t0, tf, h):
 
     #frx = np.empty((0,1))
     #fry = np.empty((0,1))
     #frz = np.empty((0,1))
 
-    for i in range (0, t):
+    for i in range (0, tf):
 
         if i == 0:
 
-            r_scal = np.linalg.norm(r)
-
-            a_x = (-mu * r_vec[0]) / (r_scal ** 3)
-            a_y = (-mu * r_vec[1]) / (r_scal ** 3)
-            a_z = (-mu * r_vec[2]) / (r_scal ** 3)
+            a_x = (-mu * r_vec[0]) / (r ** 3)
+            a_y = (-mu * r_vec[1]) / (r ** 3)
+            a_z = (-mu * r_vec[2]) / (r ** 3)
 
             a = np.array([a_x, a_y, a_z])
 
@@ -52,43 +50,44 @@ def rk4(a, r, r_vec, v_vec, t, h):
 
         else:
 
-            r_scal = np.linalg.norm(r)
+            r_scal = np.linalg.norm(r_vec)
 
             #index_x = 0 + (3)
             #index_y = 1 + (3)
             #index_z = 2 + (3)
 
-            a_x = (-mu * r[i, 0]) / (r_scal ** 3)
-            a_y = (-mu * r[i, 1]) / (r_scal ** 3)
-            a_z = (-mu * r[i, 2]) / (r_scal ** 3)
+            a_x = (-mu * r_vec[0]) / (r_scal ** 3)
+            a_y = (-mu * r_vec[1]) / (r_scal ** 3)
+            a_z = (-mu * r_vec[2]) / (r_scal ** 3)
 
             a = np.array([a_x, a_y, a_z])
 
-            k1v = a * r
+            k1v = a * r_vec
             k1r = v
-            k2v = a * (r + (k1r * (h/2)))
+            k2v = a * (r_vec + (k1r * (h/2)))
             k2r = v * k1v * (h/2)
-            k3v = a * (r + (k2r * (h/2)))
+            k3v = a * (r_vec + (k2r * (h/2)))
             k3r = v * k2v * (h/2)
-            k4v = a * (r + (k3r * h))
+            k4v = a * (r_vec + (k3r * h))
 
-            print(r)
+        #print('r', r)
 
-        ft.append(t + h)
+        ft.append(t0 + (h * i))
         fr.append(r_vec + ((1/6) * (k1r + 2*k2r + 2*k3r + k4r)))
         fv.append(v_vec + ((1/6) * (k1v + 2*k2v + 2*k3v + k4v)))
 
-        r = fr[i]
+        r_vec = fr[i]
         v = fv[i]
+
+        print(r_vec)
 
         #frx = np.append(frx, r_vec[0])
         #fry = np.append(fry, r_vec[1])
         #frz = np.append(frz, r_vec[2])
 
-    return fr, fv
+    return ft, fr, fv
 
-fr, fv = rk4(a, r, r_0, v_0, 3, 1)
+ft, fr, fv = rk4(a, r, r_0, v_0, 0, 100000, 1)
 
-print(fr)
-#ax.plot(fr)
-#plot.show()
+ax.plot(fr[0], fr[1], fr[2])
+plt.show()
