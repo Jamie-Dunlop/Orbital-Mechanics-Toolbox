a5e22a8707d8af95f35ea13697106947a4f92114
Jamie-Dunlop <32314639+Jamie-Dunlop@users.noreply.github.com>
2018-01-10 13:05:43 +0000

    RK4

--- RK4Pendulum.py
+++ RK4Pendulum.py
@@ -6,34 +6,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 t0 = 0
 tf = 100
-theta0 = 0.5
+theta0 = 1
 g = 9.81
 L = 9.81
 thetadot0 = 0
-n = 1000
-h = (tf-t0)/n
+h = 0.001
+n = (tf-t0)//h
+n = int(n)
 theta = np.zeros([n])
 thetadot = np.zeros([n])
 thetadot[0] = thetadot0
 theta[0] = theta0
 t = np.linspace(t0,tf,n)
 
+def f(x):
+    return (-g/L)*x
+
 for j in range (1, n):
 
-        k1thetadot = h * ((-g/L)*theta[j-1])
-        k1theta = h * thetadot[j-1]
+        k1thetadot = f(theta[j-1])
+        k1theta = thetadot[j-1]
 
-        k2thetadot =(thetadot[j-1] + (k1thetadot * (h/2)))
-        k2theta = theta[j-1] + k1theta * (h/2)
+        k2thetadot =f(theta[j-1] + (k1thetadot * (h/2)))
+        k2theta = thetadot[j-1] + k1theta * (h/2)
 
-        k3thetadot = (thetadot[j-1] + (k2thetadot * (h/2)))
-        k3theta = theta[j-1] + k2theta * (h/2)
+        k3thetadot = f(theta[j-1] + (k2thetadot * (h/2)))
+        k3theta = thetadot[j-1] + k2theta * (h/2)
 
-        k4thetadot = (thetadot[j-1] + (k2thetadot * (h)))
-        k4theta = theta[j-1] + k2theta * (h)
+        k4thetadot = f(theta[j-1] + (k3thetadot * (h)))
+        k4theta = thetadot[j-1] + k3theta * (h)
 
-        thetadot[j] = (thetadot[j] + ((1/6) * (k1thetadot + 2*k2thetadot + 2*k3thetadot + k4thetadot)))
-        theta[j] = (theta[j] + ((1/6) * (k1theta + 2*k2theta + 2*k3theta + k4theta)))
+        thetadot[j] = (thetadot[j-1] + ((h/6) * (k1thetadot + 2*k2thetadot + 2*k3thetadot + k4thetadot)))
+        theta[j] = (theta[j-1] + ((h/6) * (k1theta + 2*k2theta + 2*k3theta + k4theta)))
 
 print (theta)
 plt.plot(t, theta)
---
+++ RK4SpringMass.py
@@ -0,0 +1,51 @@
+#Chris Lowther 06/11/2017
+#Runge-Kutta 4th order
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+t0 = 0
+tf = 50
+x0 = -0.8
+m = 1
+k = 2
+c = 0.4
+xdot0 = 0
+h = 0.001
+n = (tf-t0)//h
+n = int(n)
+x = np.zeros([n])
+xdot = np.zeros([n])
+t = np.linspace(t0,tf,n)
+
+xdot[0] = xdot0
+x[0] = x0
+
+def f(a,b):
+    return (-c*a-k*b)/m
+
+for j in range (1, n):
+
+        k1xdot = f(xdot[j-1], x[j-1])
+        k1x = xdot[j-1]
+
+        k2xdot =f(xdot[j-1] + (k1xdot * (h/2)), x[j-1] + (k1xdot * (h/2)))
+        k2x = xdot[j-1] + k1x * (h/2)
+
+        k3xdot = f(xdot[j-1] + (k2xdot * (h/2)), x[j-1] + (k2xdot * (h/2)))
+        k3x = xdot[j-1] + k2x * (h/2)
+
+        k4xdot = f(xdot[j-1] + (k3xdot * (h)), x[j-1] + (k3xdot * (h)))
+        k4x = xdot[j-1] + k3x * (h)
+
+        xdot[j] = (xdot[j-1] + ((h/6) * (k1xdot + 2*k2xdot + 2*k3xdot + k4xdot)))
+        x[j] = (x[j-1] + ((h/6) * (k1x + 2*k2x + 2*k3x + k4x)))
+
+print (x)
+plt.plot(t, x)
+plt.xlim(0)
+plt.xlabel("Time (seconds)")
+plt.ylabel("X-position (m)")
+plt.title("Spring Mass Damper System")
+plt.grid()
+plt.show()
