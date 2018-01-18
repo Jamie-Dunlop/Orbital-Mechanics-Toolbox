01f2e38dbd2358a0899d8ae51c4f87e4e8fd6fe6
chrisrlowther <chrislowther19@gmail.com>
2018-01-18 13:49:50 +0000

    Slightly better

--- RK4Orbit.py
+++ RK4Orbit.py
@@ -4,54 +4,64 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
 t0 = 0
-tf = 8640
-r0 = 42164000 #m
+tf = 86400 #s
 mu = 3.986004418e14 #m something s something
-rdot0 = 3070000 #m/s
+r0 = np.array([42164000, 0, 0]) #m
+rdot0 = np.array([0, 3070, 0]) #m/s
 h = 1
 n = (tf-t0)//h
 n = int(n)
-T = 2*math.pi*math.sqrt(r0**3/mu)
 
-r = np.zeros([n])
-rdot = np.zeros([n])
-theta = np.zeros([n])
-x = np.zeros([n])
-y = np.zeros([n])
+T = 2 * math.pi * math.sqrt (np.linalg.norm(r0) ** 3 / mu)
+
+r = [[]]
+r[0] = r0
+r.insert(1,r0)
+
+rdot = [[]]
+rdot[0] = rdot0
+rdot.insert(1, rdot0)
+
 t = np.linspace(t0,tf,n)
 
-rdot[0] = rdot0
-r[0] = r0
-x[0] = r0
-y[0] = 0
-
-def f(Rad):
-    return -mu/ Rad**2
+def f(X):
+    return (mu) * (X) / np.linalg.norm(X) ** 3
 
 for j in range (1, n):
 
         k1rdot = f(r[j-1])
         k1r = rdot[j-1]
 
+        # print('k1rdot', k1rdot)
+        # print('k1r', k1r)
+
         k2rdot =f(rdot[j-1] + (k1rdot * (h/2)))
         k2r = r[j-1] + k1r * (h/2)
+
+        # print('k2rdot', k2rdot)
+        # print('k2r', k2r)
 
         k3rdot = f(rdot[j-1] + (k2rdot * (h/2)))
         k3r = r[j-1] + k2r * (h/2)
 
+        # print('k3rdot', k3rdot)
+        # print('k3r', k3r)
+
         k4rdot = f(rdot[j-1] + (k3rdot * (h)))
         k4r = r[j-1] + k3r * (h)
 
-        rdot[j] = (rdot[j-1] + ((h/6) * (k1rdot + 2*k2rdot + 2*k3rdot + k4rdot)))
+        # print('k4rdot', k4rdot)
+        # print('k4r', k4r)
+
+        rdot[j] = (rdot[j-1] + ((h/6) * (k1rdot + 2 * k2rdot + 2 * k3rdot + k4rdot)))
         r[j] = (r[j-1] + ((h/6) * (k1r + 2*k2r + 2*k3r + k4r)))
 
-        theta[j] = (t[j]/T)*2*math.pi
+        r.insert(j, r[j])
+        rdot.insert(j, rdot[j])
 
         print (r[j])
-
-        x[j] = r[j]*math.cos(theta[j])
-        y[j] = r[j]*math.sin(theta[j])
 
 print (r)
 print (x, y)
