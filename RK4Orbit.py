445ad311d01e1a13e26ede99551a5d019fa373bd
Jamie-Dunlop <32314639+Jamie-Dunlop@users.noreply.github.com>
2018-01-22 11:49:51 +0000

    Sorta fixed

--- RK4Orbit.py
+++ RK4Orbit.py
@@ -16,6 +16,9 @@ n = int(n)
 
 T = 2 * math.pi * math.sqrt (np.linalg.norm(r0) ** 3 / mu)
 
+x=[]
+y=[]
+z=[]
 r = [[]]
 r[0] = r0
 r.insert(1,r0)
@@ -37,31 +40,34 @@ for j in range (1, n):
         # print('k1rdot', k1rdot)
         # print('k1r', k1r)
 
-        k2rdot =f(rdot[j-1] + (k1rdot * (h/2)))
-        k2r = r[j-1] + k1r * (h/2)
+        k2rdot =f(r[j-1] + (k1r * (h/2)))
+        k2r = rdot[j-1] * k1rdot * (h/2)
 
         # print('k2rdot', k2rdot)
         # print('k2r', k2r)
 
-        k3rdot = f(rdot[j-1] + (k2rdot * (h/2)))
-        k3r = r[j-1] + k2r * (h/2)
+        k3rdot = f(r[j-1] + (k2r * (h/2)))
+        k3r = rdot[j-1] * k2rdot * (h/2)
 
         # print('k3rdot', k3rdot)
         # print('k3r', k3r)
 
-        k4rdot = f(rdot[j-1] + (k3rdot * (h)))
-        k4r = r[j-1] + k3r * (h)
+        k4rdot = f(r[j-1] + (k3r * (h)))
+        k4r = rdot[j-1] * k3rdot * (h)
 
         # print('k4rdot', k4rdot)
         # print('k4r', k4r)
 
-        rdot[j] = (rdot[j-1] + ((h/6) * (k1rdot + 2 * k2rdot + 2 * k3rdot + k4rdot)))
-        r[j] = (r[j-1] + ((h/6) * (k1r + 2*k2r + 2*k3r + k4r)))
+        rdot[j] = rdot[j-1] + ((h/6) * (k1rdot + 2 * k2rdot + 2 * k3rdot + k4rdot))
+        r[j] = r[j-1] + ((h/6) * (k1r + 2*k2r + 2*k3r + k4r))
 
         r.insert(j, r[j])
         rdot.insert(j, rdot[j])
+        x.insert(j, r[j-1][0])
+        y.insert(j, r[j-1][1])
+        z.insert(j, r[j-1][2])
 
-        print (r[j])
+
 
 print (r)
 print (x, y)
