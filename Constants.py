e2918d70ee5abb5a3d49f724433944661299c1e5
chrisrlowther <chrislowther19@gmail.com>
2018-02-02 10:40:03 +0000

    Constant Script

---
+++ Constants.py
@@ -0,0 +1,13 @@
+#https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+
+mu = 3.986004418e14 # m^3s^-2
+MassEarth = 5.2793e24 #kg
+GravConst = 6.67e-11
+g = 9.798 #m/s^2
+J2 = 1082.63e-6
+
+Equatorial_radius = 6378137 #m
+Polar_radius = 6356752 #m
+Volumetric_mean_radius = 6371008 #m
+
+Rearth = 6371008 # m
