99f86c399d482e6daac5661f2cbd4df738dd80f7
Jamie-Dunlop <32314639+Jamie-Dunlop@users.noreply.github.com>
2018-02-07 18:35:02 +0000

    Test

---
+++ NumericalSolPen.py
@@ -0,0 +1,90 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import Constants
+
+#Inital conditions
+t0 = 0
+tf = 10
+theta0 = 0.1
+L = 9.81
+thetadot0 = 0
+h = 0.5
+n = (tf-t0)//h
+n = int(n)
+t = np.linspace(t0,tf,n)
+theta = np.zeros([n])
+thetar = np.zeros([n])
+thetadotr = np.zeros([n])
+thetae = np.zeros([n])
+thetadote = np.zeros([n])
+print('t',t)
+#Exact Solution
+for j in range(0,n):
+    theta[j] = theta0*math.cos(math.sqrt(Constants.g/L)*t[j])
+print ('Real',theta)
+
+#Euler
+thetadote[0] = thetadot0
+thetae[0] = theta0
+
+for j in range(1,n):
+    thetadote[j] = h*((-Constants.g/L)*thetae[j-1]) + thetadote[j-1]
+    thetae[j] = h*(thetadote[j-1]) + thetae[j-1]
+print('Euler',thetae)
+#RK4
+thetadotr[0] = thetadot0
+thetar[0] = theta0
+
+def f(x):
+    return (-Constants.g / L) * x
+
+for j in range (1, n):
+
+        k1thetadot = f(thetar[j-1])
+        k1theta = thetadotr[j-1]
+
+        k2thetadot =f(thetar[j-1] + (k1thetadot * (h/2)))
+        k2theta = thetadotr[j-1] + k1theta * (h/2)
+
+        k3thetadot = f(thetar[j-1] + (k2thetadot * (h/2)))
+        k3theta = thetadotr[j-1] + k2theta * (h/2)
+
+        k4thetadot = f(thetar[j-1] + (k3thetadot * (h)))
+        k4theta = thetadotr[j-1] + k3theta * (h)
+
+        thetadotr[j] = (thetadotr[j-1] + ((h/6) * (k1thetadot + 2*k2thetadot + 2*k3thetadot + k4thetadot)))
+        thetar[j] = (thetar[j-1] + ((h/6) * (k1theta + 2*k2theta + 2*k3theta + k4theta)))
+
+
+ExactError = ((theta-theta))
+print('Exact Error', ExactError)
+EulerError = ((thetae-theta))
+print('Euler Error', EulerError)
+RungeError =((thetar-theta))
+print('Runge-Kutta Error',RungeError)
+
+plt.figure(1)
+plt.plot(t,theta, 'r-')
+plt.plot(t,thetae, 'y-')
+plt.plot(t,thetar, 'b-')
+axes = plt.gca()
+axes.set_xlim([0, tf])
+axes.set_ylim([-0.8,0.8])
+plt.xlabel("Time (seconds)")
+plt.ylabel("X-position (m)")
+plt.title("Exact Pendulum Solution")
+plt.grid()
+
+plt.figure(2)
+plt.plot(t,ExactError, 'r-')
+plt.plot(t,EulerError, 'y-')
+plt.plot(t,RungeError, 'b-')
+axes = plt.gca()
+axes.set_xlim([0, tf])
+axes.set_ylim([-0.0001,0.0001])
+plt.xlabel("Time (seconds)")
+plt.ylabel("Percentage Error")
+plt.title("Error Comparison Pendulum")
+plt.grid()
+plt.show()
---
+++ NumericalSolSMDtest.py
@@ -0,0 +1,94 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+t0 = 0
+tf = 500
+x0 = -0.8
+m = 1
+k = 2
+c = 0.4
+xdot0 = 0
+h = 0.001
+n = (tf-t0)//h
+n = int(n)
+state = np.array([-0.8, 0])
+t = np.linspace(t0,tf,n)
+x = np.zeros([n])
+xr = np.zeros([n])
+xdot = np.zeros([n])
+xe = np.zeros([n])
+xedot = np.zeros([n])
+
+Natfreq = math.sqrt(k/m)
+Critdamp = c/(2*Natfreq*m)
+Dampfreq = Natfreq * math.sqrt(1-Critdamp**2)
+A = x0
+B = (x0 * Critdamp) / (math.sqrt(1-Critdamp**2))
+
+#Exact Solution
+for j in range(1,n):
+    x[j] = math.exp(-Critdamp*Natfreq*t[j])*(A*math.cos(Dampfreq*t[j]) + B*math.sin(Dampfreq*t[j]))
+print (x)
+
+#Euler
+xedot[0] = xdot0
+xe[0] = x0
+
+for j in range(1,n):
+    xedot[j] = h*((-c*xedot[j-1]-k*xe[j-1])/m) + xedot[j-1]
+    xe[j] = h*(xedot[j-1]) + xe[j-1]
+
+#RK4
+
+def damped_spring(t, state):
+    pos, vel = state
+    stiffness = 2
+    damping = 0.4
+    return np.array([vel, -stiffness*pos - damping*vel])
+
+def rk4(x, h, y, f):
+    k1 = h * f(x, y)
+    k2 = h * f(x + 0.5*h, y + 0.5*k1)
+    k3 = h * f(x + 0.5*h, y + 0.5*k2)
+    k4 = h * f(x + h, y + k3)
+    return x + h, y + (k1 + 2*(k2 + k3) + k4)/6.0
+
+timed = 0
+while timed < 100:
+    jr = 0
+    timed, state = rk4(timed, h, state, damped_spring)
+    xr[jr] = state[0]
+    print('state',xr[jr])
+    jr += 1
+
+
+print('state',xr)
+ExactError = ((x-x))*100
+print('Exact Error', ExactError)
+EulerError = ((xe-x))*100
+print('Euler Error', EulerError)
+RungeError =((xr-x))*100
+print('Runge-Kutta Error',RungeError)
+
+plt.figure(1)
+plt.plot(t,x, 'r-')
+plt.plot(t,xe, 'y-')
+plt.plot(t,xr, 'b-')
+axes = plt.gca()
+axes.set_xlim([0, 500])
+axes.set_ylim([-0.8,0.8])
+plt.xlabel("Time (seconds)")
+plt.ylabel("X-position (m)")
+plt.title("Exact Spring Mass Damper System")
+plt.grid()
+
+plt.figure(2)
+plt.plot(t,ExactError, 'r-')
+plt.plot(t,EulerError, 'y-')
+plt.plot(t,RungeError, 'b-')
+plt.xlabel("Time (seconds)")
+plt.ylabel("Percentage Error")
+plt.title("Error Comparison Spring Mass Damper")
+plt.grid()
+plt.show()
